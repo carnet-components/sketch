19ce193c031e38654b844ec6f0ac56d9def5e727
znerol <lo+github@znerol.ch>
2019-10-22 08:00:45 +0200

    Add carnet-window as custom element

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { CarnetDisplay } from './src/carnet-display.js';
 import { CarnetSketch } from './src/carnet-sketch.js';
+import { CarnetWindow } from './src/carnet-window.js';
 
 customElements.define(CarnetDisplay.is, CarnetDisplay);
 customElements.define(CarnetSketch.is, CarnetSketch);
+customElements.define(CarnetWindow.is, CarnetWindow);
